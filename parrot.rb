3cfb693ff9e39b3e59227b78af77c39c74c98329
John Carter <johncarter352@gmail.com>
2020-03-24 21:19:10 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,10 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(Pretty Birds!)
+def parrot('Pretty Birds!')
   puts "Squawk!"
   return "Squawk!"
 end
-
-parrot
-
-
